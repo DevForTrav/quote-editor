25a6117d9e52e034d7b273dd4b9d09eed15972aa
Travis Courtney <travis@angry-fly.com>
2023-03-09 23:19:17 -0500

    Finished the quote generator

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -4,6 +4,7 @@ class Quote < ApplicationRecord
   belongs_to :company
   belongs_to :user
   has_many :line_item_dates, dependent: :destroy
+  has_many :line_items, through: :line_item_dates
 
   scope :ordered, -> { order(id: :desc) }
 
@@ -11,4 +12,8 @@ class Quote < ApplicationRecord
   # after_update_commit -> { broadcast_replace_later_to "quotes" }
   # after_destroy_commit -> { broadcast_remove_to "quotes" }
   broadcasts_to -> (quote) { [quote.company, 'quotes'] }, inserts_by: :prepend
+
+  def total_price
+    line_items.sum(&:total_price)
+  end
 end
